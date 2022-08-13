1f6be11658ecb492f0a2f3d83269ab4967c929a5
hendrix <leandrotully@gmail.com>
2022-08-13 09:27:25 -0300

    Aprofundando no método GET

--- api/supply/route.js
+++ api/supply/route.js
@@ -14,17 +14,29 @@ route.post('/',[
     const errors = validationResult(req);
     const errorsValidator = errors.array();
      
-    if (errorsValidator.length > 0)
+    if (errorsValidator.length > 0){
       res.status(400).json({"errors":errorsValidator});
-    
-    try{
-      const supply = new Supply(req.body);  
-      await supply.store();
-      res.status(201).end();
-    }catch(error){
-      res.status(400);  
-      throw new Error("Erro no cadastro da empresa");
+    }else{    
+      try{
+        const supply = new Supply(req.body);  
+        await supply.store();
+        res.status(201).end();
+      }catch(error){
+        res.status(400);  
+        throw new Error("Erro no cadastro da empresa");
+      }
     }
+});
+
+route.get('/:id', async (req,res) => {
+  const {id} = req.params;
+
+  try{
+     let supply = new Supply({id:id});
+     res.status(200).json({data: await supply.show()});
+  }catch(error){    
+    throw new Error(error);
+  }  
 });
 
 module.exports = route;
